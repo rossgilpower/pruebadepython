961036920f7982f9e2f04eb40314a10f3c94e534
rossgilpower <rgil89@uabc.edu.mx>
2020-09-22 13:27:49 -0700

    lol

---
+++ Practica 2.py
@@ -0,0 +1,13 @@
+#from pylab import figure
+import pylab
+import networkx as nx
+G=nx.read_weighted_edgelist('soda.txt', create_using=nx.Graph())
+nx.draw(G, with_labels = True, node_size = [G.degree(i) * 100 for i in G.nodes()])
+diam= nx.diameter(G)
+node_degree = nx.degree(G)
+vString = str(node_degree)
+resultados = [node_degree, diam]
+f=open("resultados.txt","w")
+f.write(vString)
+f.close()
+pylab.show()
--- prueba.py
+++ prueba.py
@@ -1,1 +1,8 @@
-print("Hola")
+edad = 35
+edad1 = 0o43
+edad2 = 0x23
+precio = 7435.28
+verdadero = True
+falso = False
+
+print(edad, "\n"+str(edad1), "\n"+str(edad2), "\n"+str(precio), "\n"+str(verdadero), str(falso))
